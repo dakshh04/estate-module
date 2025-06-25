9970024faf082fb2f3c62d3b513020354677c332
MTT User <dakshhsingh4902@gmail.com>
2025-06-25 15:28:05 +0530

    Estate_Account Module Files Added

---
+++ addons/estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Account',
+    'version': '1.0',
+    'depends': ['estate', 'account'],
+    'author': 'Your Name',
+    'category': 'Real Estate',
+    'summary': 'Accounting integration for the Estate module',
+    'description': 'This module links the Estate module with the Accounting module.',
+    'installable': True,
+    'application': False,
+}
